620b10f0d43a8ebe3336ab2e81fa1aa1144b3e4e
Erwan Lent <erwanlent@gmail.com>
2015-01-06 16:52:50 -0500

    Added SVG animation preloader.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var signatureLoader = new Vivus('signature-loader', {type: 'oneByOne', duration: 260}, function(){
+	$('.preloader').fadeOut('slow');
+});
+
+signatureLoader.stop().reset().play();
+
 $(document).ready(function(){
 	$(function() {
 		Grid.init();
